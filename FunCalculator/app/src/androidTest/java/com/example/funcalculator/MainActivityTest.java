c77ee8f05a0e98a7b3aead3d5c70b941a2a269ab
lolandka <roli59.59@gmail.com>
2019-10-01 23:12:59 +0200

    new files uploaded

---
+++ FunCalculator/app/src/androidTest/java/com/example/funcalculator/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.funcalculator;
+
+import static org.testng.Assert.*;
+
+public class MainActivityTest {
+
+    @org.testng.annotations.Test
+    public void testOnCreate() {
+    }
+}
